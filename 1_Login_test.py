59d27014592173f4a342bb03b6e9692bb24fbb9c
Uriyann <joshuabarotea06@gmail.com>
2025-04-25 15:20:25 +0800

    new code

--- 1_Login_test.py
+++ 1_Login_test.py
@@ -2,12 +2,54 @@ import tkinter as tk
 from tkinter import *
 from tkinter import PhotoImage
 from subprocess import call
+from tkinter import messagebox
+from openpyxl import Workbook
 
 window = tk.Tk()
 window.title("Login Portal")
 window.geometry('1300x825')
 
-# Image
+# Functions
+def data_info():
+
+    username = user_entry.get()
+    password = password_entry.get()
+
+    if username and password and username != "Username" and password != "Password":
+        print("Data Entry Form:\n\n" \
+              "Username:",username,
+              "\nPassword:",password)
+        messagebox.showinfo(title= "Login Success", message= f"Welcome, {username}!")
+
+    else:
+        messagebox.showerror(title= "Error", message= "Error. Input Required")
+
+def on_username_click(event):
+    if user_entry.get() == "Username":
+        user_entry.delete(0, tk.END)
+
+def on_username_leave(event):
+    name = user_entry.get()
+    if name == "":
+        user_entry.insert(0, "Username")
+
+def on_password_click(event):
+    if password_entry.get() == "Password":
+        password_entry.delete(0, tk.END)
+
+def on_password_leave(event):
+    password = password_entry.get()
+    if password == "":
+        password_entry.insert(0, "Password")
+
+def go_signup():
+    window.destroy()
+    call(["python", "2_Sign_Up_test.py"])
+
+#-----------------------------------------------------------
+
+# --- UI ---
+# Background
 image_path = PhotoImage(file= r'.\wallhaven-85gxp2.png')
 bg_image = tk.Label(window, image= image_path)
 bg_image.place(relheight=1, relwidth=1)
@@ -23,6 +65,10 @@ Label(window, image= image, highlightbackground="black", highlightthickness=4).g
 frame = tk.Frame(window, bg= "white", highlightbackground="black", highlightthickness=4)
 frame.grid(row=0, column=1, padx=45)
 
+#-----------------------------------------------------------
+
+# Labels & Inputs
+
 # Project Name
 project_label = tk.Label(frame, text= "PROJECT", font= ("Times New Roman bold", 30), bg= "white")
 project_label.grid(row=0, column=0, sticky="n", pady= 10)
@@ -35,43 +81,23 @@ short_desc_label.grid(row=1, column=0, sticky="n")
 login_label = tk.Label(frame, text= "Log In to Project", font= ("Arial bold", 18), bg= "white")
 login_label.grid(row=2, column=0, sticky="w", pady=30, padx= 15)
 
-def on_click(event):
-    if user_entry.get() == "Username":
-        user_entry.delete(0, tk.END)
-
-def on_leave(event):
-    name = user_entry.get()
-    if name == "":
-        user_entry.insert(0, "Username")
-
 # User Entry
 user_entry = tk.Entry(frame, width=38, font= ("Arial", 15), bd=0)
 user_entry.grid(row=3, column=0)
 user_entry.insert(0, "Username")
-user_entry.bind("<FocusIn>", on_click)
-user_entry.bind("<FocusOut>", on_leave)
+user_entry.bind("<FocusIn>", on_username_click)
+user_entry.bind("<FocusOut>", on_username_leave)
 
 # Underline using Frame
 underline = tk.Frame(frame, height=2, bg="black")
 underline.grid(row=4, column=0, sticky= "new", pady=15, padx= 15, columnspan=1)
 
-#-----------------------------------------------------------
-
-def on_click(event):
-    if password_entry.get() == "Password":
-        password_entry.delete(0, tk.END)
-
-def on_leave(event):
-    password = password_entry.get()
-    if password == "":
-        password_entry.insert(0, "Password")
-
 # Password Entry
 password_entry = tk.Entry(frame, width=38, font= ("Arial", 15), bd=0)
 password_entry.grid(row=5, column=0)
 password_entry.insert(0, "Password")
-password_entry.bind("<FocusIn>", on_click)
-password_entry.bind("<FocusOut>", on_leave)
+password_entry.bind("<FocusIn>", on_password_click)
+password_entry.bind("<FocusOut>", on_password_leave)
 
 # Underline using Frame
 underline = tk.Frame(frame, height=2, bg="black")
@@ -79,8 +105,10 @@ underline.grid(row=6, column=0, sticky= "new", pady=15, padx= 15, columnspan=1)
 
 #-------------------------------------------------------------
 
+# Buttons
+
 # Button Sign in
-login_button = tk.Button(frame, text= "Log In", width=38, font= ("Arial bold", 15), bg= "dodger blue", fg= "white")
+login_button = tk.Button(frame, text= "Log In", width=38, font= ("Arial bold", 15), bg= "dodger blue", fg= "white", command= data_info)
 login_button.grid(row=7, column=0, pady=15, padx= 15)
 
 # Remember Me Checkbutton
@@ -96,11 +124,7 @@ forgpass_label.grid(row=8, column=0, sticky="e", padx= 15)
 underline = tk.Frame(frame, height=2, bg="black")
 underline.grid(row=9, column=0, sticky= "new", pady=15, padx= 15, columnspan=1)
 
-def go_signup():
-    window.destroy()
-    call(["python", "2_Sign_Up_test.py"])
-
-
+# SIGNUP Button
 create_label = tk.Button(frame, text="Need an Account? SIGN UP", font=("Arial", 12), bg= "white", borderwidth= 0, command=go_signup)
 create_label.grid(row=10, column=0, sticky="n", pady=15)
 
