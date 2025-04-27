1442d72eb91e14ff791985ddbc7b758686171287
Uriyann <joshuabarotea06@gmail.com>
2025-04-27 19:56:46 +0800

    fixed

--- 1_Login_Test_CTK.py
+++ 1_Login_Test_CTK.py
@@ -12,14 +12,13 @@ window.geometry('1300x825')
 window.resizable(False, False)
 
 # ==================== Functions ====================
-
 # Login Checker Function
 def login_checker():
-    if not data_validation_debugger():
+    if not log_in_data_validation_debugger():
         return
 
-    username = user_entry.get()
-    password = password_entry.get()
+    username = login_user_entry.get()
+    password = login_password_entry.get()
 
     try:
         wb = load_workbook("user_account_data.xlsx")
@@ -36,10 +35,10 @@ def login_checker():
         messagebox.showerror(title= "Error", message= "User data file not found.")
 
 # Input Validation & Debugger Function
-def data_validation_debugger():
+def log_in_data_validation_debugger():
 
-    username = user_entry.get()
-    password = password_entry.get()
+    username = login_user_entry.get()
+    password = login_password_entry.get()
 
     if username and password and username != "Username" and password != "Password":
         print("\n\nData Entry Form:\n\n" \
@@ -53,34 +52,34 @@ def data_validation_debugger():
         return False
 
 # Username Delete & Restore Function
-def on_username_click(event):
-    if user_entry.get() == "Username":
-        user_entry.delete(0, 'end')
+def log_in_on_username_click(event):
+    if login_user_entry.get() == "Username":
+        login_user_entry.delete(0, 'end')
 
-def on_username_leave(event):
-    name = user_entry.get()
+def log_in_on_username_leave(event):
+    name = login_user_entry.get()
     if name == "":
-        user_entry.insert(0, "Username")
+        login_user_entry.insert(0, "Username")
 
 # Password Delete & Restore Function
-def on_password_click(event):
-    if password_entry.get() == "Password":
-        password_entry.delete(0, END)
+def log_in_on_password_click(event):
+    if login_password_entry.get() == "Password":
+        login_password_entry.delete(0, END)
 
-def on_password_leave(event):
-    password = password_entry.get()
+def log_in_on_password_leave(event):
+    password = login_password_entry.get()
     if password == "":
-        password_entry.insert(0, "Password")
+        login_password_entry.insert(0, "Password")
 
 # Signup Window Switch Function
 def go_signup():
     window.destroy()
-    call(["python", "2_Sign_Up_test.py"])
-    pass
+    call(["python", "2_Sign_Up_Test_CTK.py"])
 
-def handle_enter(event):
+def log_in_handle_enter(event):
     if not login_checker():
         return
+    login_button.invoke()
 
 # //////////////////////////////////////////////////////////
 
@@ -99,67 +98,65 @@ side_label.place(relx = 0.5, rely = 0.5, x= -547, y= -268)
 # //////////////////////////////////////////////////////////
 
 # ==================== Frames ====================
-frame = CTkFrame(window, border_width= 3, corner_radius= 15)
-frame.place(relx = 0.5, rely = 0.5, x= 90, y= -200)
+log_in_frame = CTkFrame(window, border_width= 3, corner_radius= 15)
+log_in_frame.place(relx = 0.5, rely = 0.5, x= 90, y= -200)
 
 # //////////////////////////////////////////////////////////
 
 # ==================== Labels & Inputs ====================
-
 # Project Name
-project_label = CTkLabel(frame, text= "PROJECT UniPass", font= ("Times New Roman bold", 40))
-project_label.grid(row=0, column=0, sticky="n", pady= 10, padx= 15)
+login_project_label = CTkLabel(log_in_frame, text= "PROJECT UniPass", font= ("Times New Roman bold", 40))
+login_project_label.grid(row=0, column=0, sticky="n", pady= 10, padx= 15)
 
 # Short Description
-short_desc_label =  CTkLabel(frame, text= "/Short Description/", font= ("Helvetica bold", 18))
+short_desc_label =  CTkLabel(log_in_frame, text= "/Short Description/", font= ("Helvetica bold", 18))
 short_desc_label.grid(row=1, column=0, sticky="n")
 
 # Login
-login_label = CTkLabel(frame, text= "Log In to Project", font= ("Helvetica bold", 17))
+login_label = CTkLabel(log_in_frame, text= "Log In to Project", font= ("Helvetica bold", 17))
 login_label.grid(row=2, column=0, sticky="w", pady=15, padx= 15)
 
 # User Entry
-user_entry = CTkEntry(frame, font= ("Arial", 16), border_width=0, width=400, placeholder_text="Username")
-user_entry.grid(row=3, column=0, sticky= "n", pady=15, padx= 15)
-user_entry.bind("<FocusIn>", on_username_click)
-user_entry.bind("<FocusOut>", on_username_leave)
-user_entry.bind('<Return>', handle_enter)
+login_user_entry = CTkEntry(log_in_frame, font= ("Arial", 16), border_width=0, width=400, placeholder_text="Username", height= 35)
+login_user_entry.grid(row=3, column=0, sticky= "n", pady=15, padx= 15)
+login_user_entry.bind("<FocusIn>", log_in_on_username_click)
+login_user_entry.bind("<FocusOut>", log_in_on_username_leave)
+login_user_entry.bind('<Return>', log_in_handle_enter)
 
 # Divider Line
-user_line = CTkFrame(frame, width=400, height=2, fg_color="white")
-user_line.place(x=15, y=197)
+user_line = CTkFrame(log_in_frame, width=400, height=2, fg_color="white")
+user_line.place(x=15, y=203)
 
 # Password Entry
-password_entry = CTkEntry(frame, font= ("Arial", 16), border_width=0, width=400, placeholder_text="Password", show="*")
-password_entry.grid(row=4, column=0, sticky= "n", pady=15, padx= 15)
-password_entry.bind("<FocusIn>", on_password_click)
-password_entry.bind("<FocusOut>", on_password_leave)
-user_entry.bind('<Return>', handle_enter)
-
+login_password_entry = CTkEntry(log_in_frame, font= ("Arial", 16), border_width=0, width=400, placeholder_text="Password", show="*", height= 35)
+login_password_entry.grid(row=4, column=0, sticky= "n", pady=15, padx= 15)
+login_password_entry.bind("<FocusIn>", log_in_on_password_click)
+login_password_entry.bind("<FocusOut>", log_in_on_password_leave)
+login_password_entry.bind('<Return>', log_in_handle_enter)
 
 # Divider Line
-pass_line = CTkFrame(frame, width=400, height=2, fg_color="white")
-pass_line.place(x=15, y=255)
+pass_line = CTkFrame(log_in_frame, width=400, height=2, fg_color="white")
+pass_line.place(x=15, y=268)
 
 # //////////////////////////////////////////////////////////
 
 # ==================== Buttons ====================
 
 # Button Login
-login_button = CTkButton(master=frame, text= "Log In", width=400, font= ("Arial bold", 15), command= login_checker)
+login_button = CTkButton(log_in_frame, text= "Log In", width=325, font= ("Arial bold", 15), command= login_checker, height= 35)
 login_button.grid(row=5, column=0, pady=15, padx= 15)
 
 # Signup Text + Button
-signup_frame = CTkFrame(frame)
-signup_frame.grid(row=6, column=0, pady=20)
+sign_frame = CTkFrame(log_in_frame)
+sign_frame.grid(row=6, column=0, pady=20)
 
-need_account_label = CTkLabel(signup_frame, text="Need an Account?", font=("Arial", 12))
+need_account_label = CTkLabel(sign_frame, text="Need an Account?", font=("Arial", 12))
 need_account_label.grid(row=0, column=0, padx=10)
 
-signup_button = CTkButton(signup_frame, text="SIGN UP", font=("Arial", 12), fg_color="transparent", hover_color="lightblue", text_color="dodgerblue2")
-signup_button.grid(row=0, column=1, padx=5)
+sign_up_button = CTkButton(sign_frame, text="SIGN UP", font=("Arial", 12), fg_color="transparent", hover_color="lightblue", text_color="dodgerblue2", command= go_signup, width=50)
+sign_up_button.grid(row=0, column=1, padx=5)
 
 # //////////////////////////////////////////////////////////
 
-
+# ==================== Window Starter ====================
 window.mainloop()
